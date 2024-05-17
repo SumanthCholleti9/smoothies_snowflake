174272ac0931ef888e350b9b1ffe4eb16bbc3c25
SumanthCholleti9 <157079955+SumanthCholleti9@users.noreply.github.com>
2024-05-17 18:03:08 -0400

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -34,7 +34,8 @@ if ingredient_list:
     ingredients_string= ''
     for fruit_chosen in ingredient_list:
         ingredients_string += fruit_chosen + ' '
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        st.subheader(fruit_chosen + 'Nutritiona Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon" + fruit_chosen)
         fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
         name_on_order=name
         contact_details=Contact
